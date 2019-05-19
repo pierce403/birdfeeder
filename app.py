62ee7080d6705c493823038acb0944c2d8a6b734
Dean Pierce <pierce403@gmail.com>
2019-05-19 03:16:28 -0400

    one more leading slash

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import request
 import time
 import flask
 from flask import Flask
-app = Flask(__name__,static_url_path='static')
+app = Flask(__name__,static_url_path='/static')
 
 import os
 import requests
