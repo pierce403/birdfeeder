32b66e25ce91ac81af2645d1f838fb367ff4f422
Dean Pierce <pierce403@gmail.com>
2019-05-19 03:18:08 -0400

    too many leading slashes

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ import tweepy as tw
 
 from flask import send_from_directory
 
-@app.route('/favicon.ico')
+@app.route('favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, '/static'),
+    return send_from_directory(os.path.join(app.root_path, 'static'),
                           'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 @app.route('/', methods=("GET", "POST", "OPTIONS"))
