57dc6e3c5a6c31398d0b9856d7b7bdc985df1b3a
Dean Pierce <pierce403@gmail.com>
2019-05-19 03:13:42 -0400

    leading slash

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from flask import send_from_directory
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
+    return send_from_directory(os.path.join(app.root_path, '/static'),
                           'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 @app.route('/', methods=("GET", "POST", "OPTIONS"))
